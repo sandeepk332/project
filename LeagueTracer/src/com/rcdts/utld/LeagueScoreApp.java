e785e679400768ca3bcbcd91f3d1930b1f90db9a
Sandeep <sandeep_waterford@yahoo.com>
2018-05-18 00:11:29 +0100

    Exception added

--- LeagueTracer/src/com/rcdts/utld/LeagueScoreApp.java
+++ LeagueTracer/src/com/rcdts/utld/LeagueScoreApp.java
@@ -34,68 +34,6 @@ public class LeagueScoreApp {
 
 	
 
-//	
-//
-//	interface ScoreSplitter {
-//
-//		public String[] getEachScoreFromInput(String allScores);
-//
-//	};
-//
-//	ScoreSplitter scoreSplitter = (String argument) -> {
-//
-//		String[] eachScore = argument.split(",");
-//
-//		return eachScore;
-//
-//	};
-//
-//	interface ScoreCalculator {
-//
-//		public int calculateScore(int previousTotalScore, String eachScore);
-//
-//	}
-//
-//	ScoreCalculator scoreCalculatorRCDTSUtld = (int previousTotalScore, String eachScore) -> {
-//
-//		System.out.println("in loop:" + previousTotalScore);
-//
-//		System.out.println("in loop each:" + eachScore);
-//
-//		totalScore = previousTotalScore;
-//
-//		String trimSc = eachScore.trim();
-//
-//		String[] twoScore = trimSc.split("-");
-//
-//		int hometeamScore = Integer.parseInt(twoScore[0]);
-//
-//		int otherteamScore = Integer.parseInt(twoScore[1].substring(0, 1));
-//
-//		if (otherteamScore == hometeamScore) {
-//
-//			totalScore = totalScore + draw_score;
-//
-//		}
-//
-//		else if (otherteamScore < hometeamScore) {
-//
-//			totalScore = totalScore + win_score;
-//
-//		}
-//
-//		return totalScore;
-//
-//	};
 
-	
-
-	/**
-	 * 
-	 * @param args
-	 * 
-	 */
-
-	// TODO Auto-generated method stub
 
 }
